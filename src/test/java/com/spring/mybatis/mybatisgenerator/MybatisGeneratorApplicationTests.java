f32193648dd5a14bdb179de20c125cb5eafa371f
darly <zhangyi@mail.ynu.edu.cn>
2021-02-18 12:39:13 +0800

    mybatis-generator自动生成sql语句

---
+++ src/test/java/com/spring/mybatis/mybatisgenerator/MybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mybatis.mybatisgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
